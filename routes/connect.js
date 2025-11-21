f2b51d6bcc893c63b7f21d1491fd31bcfa21c4dc
PichetDotK <pckjoke@gmail.com>
2025-11-21 10:36:04 +0700

    Update connect.js

--- routes/connect.js
+++ routes/connect.js
@@ -1,5 +1,5 @@
 const mongoose = require('mongoose');
-  //เครื่อง รพสต. pichet002 เครื่องที่บ้าน pichet68
+  // [เครื่อง รพสต. password:pichet002] [เรื่องที่บ้าน password:pichet68]
 mongoose.connect('mongodb://superadmin:pichet002@localhost:27017/karupanDB', {
   authSource: 'admin' // ระบุฐานข้อมูลที่ใช้ตรวจสอบสิทธิ์
 })
