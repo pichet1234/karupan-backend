98dc108ec13dc90d1d74b3550012cb18bdaf9685
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2025-11-26 07:22:17 +0700

    update pass

--- routes/connect.js
+++ routes/connect.js
@@ -1,6 +1,6 @@
 const mongoose = require('mongoose');
   // [เครื่อง รพสต. password:pichet002] [เรื่องที่บ้าน password:pichet68]
-mongoose.connect('mongodb://superadmin:pichet002@localhost:27017/karupanDB', {
+mongoose.connect('mongodb://superadmin:pichet68@localhost:27017/karupanDB', {
   authSource: 'admin' // ระบุฐานข้อมูลที่ใช้ตรวจสอบสิทธิ์
 })
 .then(() => {
