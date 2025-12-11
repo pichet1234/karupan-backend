7881ea7ef44040803f04a425ebdc37e987ae505f
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2025-12-11 20:06:23 +0700

    create schema karupans

---
+++ routes/schema/karupans.js
@@ -0,0 +1,15 @@
+var mongoose = require('../connect');//เชื่อมต่อฐานข้อมูล
+var karupansSchema = new mongoose.Schema({
+    kname: String,
+    karupantype: String,
+    detail: String,
+    price: Number,
+    station: String,
+    expenditure: String,
+    usefullife: String,
+    status:String,
+    brand: String,
+    imageUrl: String,
+},{ collection: 'karupans' });
+
+module.exports = karupansSchema;
