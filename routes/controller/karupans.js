1c6145f943bbceb68610e0e5c610e8f207643797
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2025-12-23 06:57:53 +0700

    update collection

--- routes/controller/karupans.js
+++ routes/controller/karupans.js
@@ -3,7 +3,8 @@ var karupans = mongoose.model('karupans',require('../schema/karupans'));
 var regitdonate = mongoose.model('regitdonate',require('../schema/regitdonate'));
 
 module.exports = { 
-    addkarupans: async (req, res) => { // แบบทั่วไป
+    //=============================== แบบทั่วไป =================================//
+    addkarupans: async (req, res) => { 
         try{
             let imageUrl = '';
             if (req.file) {
@@ -28,13 +29,15 @@ module.exports = {
             res.status(500).json({ message: 'Server Error', error: error.message });
         }
     },
-    donatekarupan: async (req, res) => { // สำหรับบริจาค
+    //=============================== สำหรับบริจาค =================================//
+    donatekarupan: async (req, res) => { 
         try {
-                        let imageUrl = '';
+            //===== ส่วนนี้ลง collection karupans =====
+            let imageUrl = '';
             if (req.file) {
               imageUrl = `uploads/${req.file.filename}`;
             }
-                        const apidata = await karupans.create({
+                const apidata = await karupans.create({
                 kname: req.body.kname,
                 karupantype: req.body.karupantype,
                 karupanCode: req.body.karupanCode,
@@ -48,29 +51,20 @@ module.exports = {
                 brand: req.body.brand,
                 imageUrl: imageUrl
              });
-             return res.status(201).json({ message: 'karupans added successfully', data: apidata });
+
+            //====== ส่วนนี้ลง collection regitdonate ======
              if (apidata && apidata._id) {
-                await regitdonate.create({ 
-                    karupanid: apidata._id,
-                    donate_date: new Date(),
-                    name: req.body.name,
-                    address: req.body.address,
-                    tel: req.body.tel,
-                    kname: req.body.kname,
-                    karupantype: req.body.karupantype,
-                    karupanCode: req.body.karupanCode,
-                    redate: req.body.redate,
-                    detail: req.body.detail,
-                    price: req.body.price,
-                    station: req.body.station,
-                    expenditure: req.body.expenditure,
-                    usefullife: req.body.usefullife,
-                    status: req.body.status,
-                    brand: req.body.brand,
-                    imageUrl: imageUrl
-                });
+                 await regitdonate.create({ 
+                     karupanid: apidata._id,
+                     donate_date: new Date(),
+                     name: req.body.name,
+                     address: req.body.address,
+                     tel: req.body.tel
+                    });
+                return res.status(201).json({ message: 'karupans added successfully', data: apidata });
              }
         } catch (error) { 
+            res.status(500).json({ message: 'Server Error', error: error.message });
         }
     }
 }
