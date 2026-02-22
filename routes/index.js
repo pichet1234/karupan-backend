ab9f056d61fec5b46723b71d5cf35741ebf5491a
PichetDotK <146558868+pichet1234@users.noreply.github.com>
2026-02-22 22:41:48 +0700

    Update index

--- routes/index.js
+++ routes/index.js
@@ -72,7 +72,7 @@ router.get('/countborrowstatus', (req, res, next)=>{ borrow.countStatusone(req, 
 router.get('/countborrowstatussuccess', (req, res, next)=>{ borrow.countStatustwo(req, res);});//นับจำนวนการยืมที่คืนสำเร็จ
 router.get('/countborrowall', (req, res, next)=>{ borrow.countborrowAll(req, res);});//นับจำนวนการยืมทั้งหมด
 router.get('/getfinanceLogs', (req, res, next)=> { financeLogs.getAllFinanceLogs(req, res); });//ดึงข้อมูลบัญชีการเงินทั้งหมด
-router.delete('/removekarupann/:id', (req, res, next)=>{ karupans.deletekarupan(req, res);});//ลบครุภัณฑ์
+router.delete('/removekarupan/:id', (req, res, next)=>{ karupans.deletekarupan(req, res);});//ลบครุภัณฑ์
 router.post('/updatekarupan', upload.single('file'), (req, res, next)=>{ karupans.updateKarupan(req, res);});//แก้ไขครุภัณฑ์
 router.get('/countkborrw', (req, res, next) =>{ karupans.countkborrow(req,res);});
 
